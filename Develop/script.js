b0299a8a56acbfa54878c62701fa38488a8601f4
cardinal <caren@merzhaus.org>
2022-05-21 11:34:14 -0400

    troubleshoot color

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,37 @@
-// current day is displayed at the top of the calendar
+var date = new Date();
+
+// today's date appears in header
+$("#currentDay").text(date.toLocaleDateString());
+
+var today = moment().format("dddd, MMMM Do");
+var now = moment().format("H A");
+var rows = $(".time-block");
+var currentHour = date.getHours();
+
+// var data = getData();
+
+let foundcurrenthour = false;
+
+$.each(rows, function(i, row) {
+    if (row.id === "Hour" + currentHour) {
+        //highlight present hours in red
+        $(row).find("textarea").addClass("present");
+        foundcurrenthour = true;
+    } else {
+        //if statement to highlight future hours in green
+        if (foundcurrenthour) {
+            $(row).find("textarea").addClass("future");
+            //else if statement to highlight past hours in grey
+        } else {
+            $(row).find("textarea").addClass("past");
+        }
+    }
+})
+
+//click  save button for that time block
 
 
+//text for that event is saved in local storage
 
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-//current day and hour
-var hour = moment().format('h:mm:ss a');
-var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
-var userInput;
-var hourSpan;
+//refresh the page, the saved events persist
